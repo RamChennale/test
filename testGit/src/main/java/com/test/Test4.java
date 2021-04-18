30f9a0cf8ded67c8f8639bbd41f6801de2cd6f28
Ram <chennaleram@gmail.com>
2021-04-18 16:35:44 +0530

    Age validation method added, please review.

--- testGit/src/main/java/com/test/Test4.java
+++ testGit/src/main/java/com/test/Test4.java
@@ -1,8 +1,18 @@
 package com.test;
 
 public class Test4 {
+	
+	int ageEle=18;
 
 	public static void main(String args[]) {
 		System.out.println("Hey I Am Test4");
+	}
+	
+	public void verifyVoterEligibility() {
+		if(ageEle>=18)
+			System.out.println("We welcome you: ECI");
+		else {
+			System.out.println("You are not eligible: ECI");
+		}
 	}
 }
